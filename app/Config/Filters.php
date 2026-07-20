467929824ea6661135e8e2f44a14427089bd3734
Youssi <andrianoayoussi@gmail.com>
2026-07-20 13:22:23 +0300

    db

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -26,7 +26,7 @@ class Filters extends BaseConfig
             // 'honeypot',
             'invalidchars',
             'secureheaders',
-            'auth' => ['except' => ['operator/prefixes', 'login', 'register', 'auth/*']],
+            'auth' => ['except' => [ 'login', 'register', 'auth/*']],
         ],
         'after' => [
             'toolbar',
